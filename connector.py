377d45bbd8595d48df20d7e4e972984560125112
Andrew DeAngelis <andrew.deangelis@paperlessparts.com>
2025-01-30 16:08:54 -0500

    update to absolute pathing in connector.py

--- connector.py
+++ connector.py
@@ -1,4 +1,9 @@
-from integrations.baseintegration.utils import run_integration
+import os
+import sys
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), "integrations"))
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), "integrations/epicor"))
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), "integrations/baseintegration"))
+from baseintegration.utils import run_integration
 
 if __name__ == '__main__':
     run_integration()
